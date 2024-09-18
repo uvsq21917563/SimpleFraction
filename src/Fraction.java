c95bf24bf534b729e1eb2495c9f3d1d4a4a24303
Clément Richard <146951644+uvsq21917563@users.noreply.github.com>
2024-09-18 14:25:17 +0200

    declaration des variables

--- src/Fraction.java
+++ src/Fraction.java
@@ -2,6 +2,14 @@ package src;
 
 public class Fraction {
 
+    private int dénominateur;
+    private int numérateur;
+
+    public Fraction(int numérateur, int dénominateur) {
+        this.numérateur = numérateur;
+        this.dénominateur = dénominateur;
+    }
+    
     public String toString() {
         return "Je suis une fraction.";
     }
